b18f0a59a15b12c9eb743559184aa124a2c3f857
cyny666 <113816042+cyny666@users.noreply.github.com>
2024-04-02 20:18:23 +0800

    Revert "invitation"

--- main.go
+++ main.go
@@ -3,38 +3,39 @@ package main
 import (
 	"A-Secure-File-Sharing-System/client"
 	"fmt"
-	"log"
-
-	"fyne.io/fyne/v2/app"
 )
 
-func main() {
-	// 初始化测试用户
-	client.InitUser("test", "test")
-
-	// 创建应用程序
-	fyneApp := app.NewWithID("test")
-
-	// 创建登录界面
-	err := makeLogin()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// 运行应用程序
-	fyneApp.Run()
-}
+//func main() {
+//	// 初始化测试用户
+//	client.InitUser("test", "test")
+//
+//	// 创建应用程序
+//	fyneApp := app.NewWithID("test")
+//
+//	// 创建登录界面
+//	err := makeLogin()
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	// 运行应用程序
+//	fyneApp.Run()
+//}
 
 // 这里要写一个shell的界面以方便用户交互
-func main_v1() {
-	user1, erro := client.InitUser("cyny666", "123456")
-	if erro != nil {
-		fmt.Print(erro)
+func main() {
+	// 初始化用户
+	user1, err := client.InitUser("cyny666", "123456")
+	if err != nil {
+		fmt.Print(err)
+		return
 	}
-	user2, erro2 := client.GetUser("cyny666", "123456")
-	if erro2 != nil {
-		fmt.Println(erro2)
+
+	// 调用 StoreFile 函数
+	err = user1.StoreFile("example.txt", []byte("Hello, world!"))
+	if err != nil {
+		fmt.Print(err)
 	}
-	fmt.Print(user1)
-	fmt.Println(user2)
+	content, _ := user1.LoadFile("example.txt")
+	fmt.Print(string(content))
 }
